c541d59b1eebc096e370c4e8358f58b212753a9e
Callum Andrew <callum@cmaconsulting.co.nz>
2024-02-15 07:54:42 +1300

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener('scroll', function() {
-    var logo = document.getElementById('logo');
+    var logoContainer = document.querySelector('.logo-container');
     var size = Math.max(60, 100 - window.pageYOffset / 5);
-    logo.style.height = size + 'px';
+    logoContainer.style.height = size + 'px';
 });
+
